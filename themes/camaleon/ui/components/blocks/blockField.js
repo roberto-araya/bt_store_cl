9a38957c0b884e541e57275cde901bf9b70480d0
Roberto Araya <roberto.araya@btester.cl>
2023-07-26 19:22:42 -0400

    Update Camaleon blockFieldBlock.

---
+++ themes/camaleon/ui/components/blocks/blockField.js
@@ -0,0 +1,11 @@
+((Camaleon) => {
+  blockFieldBlock = function(blocks, breakpoint) {
+    let i = 0
+    for (i; i < blocks.length; i++) {
+      Camaleon.applyProperties(blocks[i], breakpoint)
+    }
+  }
+  
+  Camaleon.blocksRegister('blockFieldBlock', blockFieldBlock)
+
+})(Camaleon)
