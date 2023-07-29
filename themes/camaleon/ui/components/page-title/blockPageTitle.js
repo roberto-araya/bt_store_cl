3d8d27f505963f12e47527587ef9dc3ba72cd34a
Roberto Araya <roberto.araya@btester.cl>
2023-07-29 09:19:46 -0400

    Add page title block style to Camaleon theme.

---
+++ themes/camaleon/ui/components/page-title/blockPageTitle.js
@@ -0,0 +1,15 @@
+((Camaleon) => {
+  blockPageTitle = function(blocks, breakpoint) {
+    let customProperties = {
+      TitleFontSize: 'titleFontSize',
+    }
+
+    let i = 0
+    for (i; i < blocks.length; i++) {
+      Camaleon.applyProperties(blocks[i], breakpoint, customProperties)
+    }
+  }
+  
+  Camaleon.blocksRegister('blockPageTitle', blockPageTitle)
+
+})(Camaleon)
