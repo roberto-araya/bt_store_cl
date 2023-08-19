e2de57b0ea5f415dcedd843a9919db34321bd989
Roberto Araya <roberto.araya@btester.cl>
2023-08-19 07:27:42 -0400

    Add branding information.

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\bt_store_cl\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * About page controller.
+ */
+class AboutController extends ControllerBase {
+
+  /**
+   * Returns a render-able array for the about project page.
+   */
+  public function content() {
+    return [
+      '#markup' => '<p class="text-align-center"><strong>TiendaParaMiPyme</strong> es una distribución <a href="https://www.drupal.org">Drupal</a> de comercio electrónico específica para Chile, para más información, ayuda y soporte visite nuestro sitio web <a href="https://www.tiendaparamipyme.cl"><strong>www.tiendaparamipyme.cl</strong></a>.</p>'
+    ];
+  }
+
+}
