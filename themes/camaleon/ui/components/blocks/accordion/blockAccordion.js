3cd300b9aa1f4478edd8ef778231dbf4e86058a1
Roberto Araya <roberto.araya@btester.cl>
2023-08-31 08:53:45 -0400

    Update Camaleon theme.

---
+++ themes/camaleon/ui/components/blocks/accordion/blockAccordion.js
@@ -0,0 +1,17 @@
+((Camaleon) => {
+  blockAccordion = function(blocks, breakpoint) {
+    let customProperties = {
+      TitleFontSize: 'titleFontSize',
+      CaptionFontSize: 'captionFontSize',
+      TextFontSize: 'textFontSize',
+    }
+
+    let i = 0
+    for (i; i < blocks.length; i++) {
+      Camaleon.applyProperties(blocks[i], breakpoint, customProperties)
+    }
+  }
+  
+  Camaleon.blocksRegister('blockAccordion', blockAccordion)
+
+})(Camaleon)
---
+++ themes/camaleon/ui/components/blocks/faq/blockFaq.js
@@ -0,0 +1,17 @@
+((Camaleon) => {
+  blockFaq = function(blocks, breakpoint) {
+    let customProperties = {
+      TitleFontSize: 'titleFontSize',
+      CaptionFontSize: 'captionFontSize',
+      TextFontSize: 'textFontSize',
+    }
+
+    let i = 0
+    for (i; i < blocks.length; i++) {
+      Camaleon.applyProperties(blocks[i], breakpoint, customProperties)
+    }
+  }
+  
+  Camaleon.blocksRegister('blockFaq', blockFaq)
+
+})(Camaleon)
