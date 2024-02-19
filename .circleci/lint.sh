d60293b16ae827a9eb82d71e7e50713d5b23beca
Roberto Araya <roberto.araya@btester.cl>
2024-02-19 10:17:50 -0300

    Update lint script.

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -24,12 +24,13 @@ build/vendor/bin/phpcs \
   -p \
   --standard=Drupal,DrupalPractice \
   --extensions=module,php,install,inc,test,info.yml,js \
+  --ignore=*.min.js \
   "build/web/profiles/${MODULE}"
 
-#echo "  > Running drupal-check."
-#build/vendor/bin/drupal-check \
-#  --drupal-root=build/web \
-#  "build/web/modules/${MODULE}"
+echo "  > Running drupal-check."
+build/vendor/bin/drupal-check \
+  --drupal-root=build/web \
+  "build/web/profiles/${MODULE}"
 
 echo "  > Running Drupal Rector."
 pushd "build" >/dev/null || exit 1
